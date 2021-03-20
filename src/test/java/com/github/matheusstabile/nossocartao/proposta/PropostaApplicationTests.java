d983f40cfc92781d34853e1c1a53d0cbc4ef6206
Matheus Stabile <matheus.stabile@gmail.com>
2021-03-20 01:51:53 -0300

    Commit Inicial

---
+++ src/test/java/com/github/matheusstabile/nossocartao/proposta/PropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.matheusstabile.nossocartao.proposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
